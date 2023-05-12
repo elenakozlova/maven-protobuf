2730da82c5c1410cfa667283fafa6380134be6ef
elena.kozlova <elena.kozlova@jetbrains.com>
2023-05-12 18:20:12 +0200

    Initial commit

---
+++ src/main/java/org/example/app/Application.java
@@ -0,0 +1,27 @@
+package org.example.app;
+
+import org.example.protobuf.AddressBookProtos;
+
+import java.util.Random;
+
+public class Application {
+    public static void main(String[] args) {
+        int id = new Random().nextInt();
+        String name = "Test";
+        String email = "test@example.org";
+        String number = "01234567890";
+
+        AddressBookProtos.Person person = AddressBookProtos.Person.newBuilder()
+                .setId(id)
+                .setName(name)
+                .setEmail(email)
+                .addNumbers(number)
+                .build();
+
+        AddressBookProtos.AddressBook addressBook = AddressBookProtos.AddressBook.newBuilder()
+                .addPeople(person)
+                .build();
+
+        System.out.println(addressBook);
+    }
+}
